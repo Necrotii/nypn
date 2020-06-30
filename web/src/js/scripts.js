ac794ed352c4ce1ad5cb8dc881a521364998fd1e
Peter Boyer <blackrider2468@hotmail.com>
2020-06-30 17:15:05 -0500

    Added mobile hamburger and fixed some bugs

--- web/src/js/scripts.js
+++ web/src/js/scripts.js
@@ -1,3 +1,19 @@
 $(function() {
     $(document).foundation();
 });
+
+function showNav() {
+    //var container = document.getElementsByClassName("mobile-button-container hide");
+    var container = document.getElementById("mbc");
+    var head = document.getElementById("head")
+
+    if (container.className === "mobile-button-container border hidden") {
+        container.className = "mobile-button-container border";
+        head.className = "header"
+        document.getElementById("cheems").src = "src/img/hamburger_close.png";
+    } else {
+        container.className = "mobile-button-container border hidden";
+        head.className = "header border"
+        document.getElementById("cheems").src = "src/img/hamburger.png";
+    }
+}
